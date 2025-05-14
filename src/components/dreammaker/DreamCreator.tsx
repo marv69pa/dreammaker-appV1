a7118e85c7bda424d67dfd2048468819e19932ad
marv69pa <52975567+marv69pa@users.noreply.github.com>
2025-05-13 20:31:10 -0400

    Update DreamCreator.tsx

--- src/components/dreammaker/DreamCreator.tsx
+++ src/components/dreammaker/DreamCreator.tsx
@@ -1,3 +1,4 @@
+import { generateDreamContent } from '@/lib/generateDream';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDream } from '@/contexts/DreamContext';
